e4a1234ca44b9db592ed058861d141b22b081924
wouter173 <wouter173@outlook.com>
2020-12-14 17:52:02 +0100

    Dotfiles #1

---
+++ setup.sh
@@ -0,0 +1,6 @@
+apt install zsh
+apt install neofetch
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm -r .git
+rm -- "$0"
