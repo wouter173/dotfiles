9fd129f000aff6aebfbb6bbb7b63d723f59dac2a
wouter <31807225+wouter173@users.noreply.github.com>
2021-03-09 22:19:15 +0100

    updated aliases.sh

--- misc/aliases.sh
+++ misc/aliases.sh
@@ -1,3 +1,24 @@
+# fun zone
 alias nano='micro'
 alias vim='echo Nope'
 alias vi='vim'
+
+#directory aliases
+alias ls='lsd -a'
+alias la='lsd -la'
+
+alias cdd='cd ..'
+
+# Git aliases
+alias g='git'
+alias ga='git add'
+alias gs='git status'
+alias gb='git branch'
+alias gc='git commit -v'
+alias gd='git diff'
+alias gp='git push'
+alias gr='git restore'
+alias gf='git fetch -p'
+alias gco='git checkout'
+alias glo="git log --date=short --color --graph --pretty=format:'%Cgreen[%Creset%h%Cgreen]%Creset -%C(yellow)%d%Creset %s %Cgreen(%cd) %C(bold blue)%Creset' --abbrev-commit"
+alias glc='git log --oneline | wc -l'
