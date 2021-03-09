8b8dc8bd4f505406139e00c697f96853cfdb02ba
wouter <31807225+wouter173@users.noreply.github.com>
2021-03-09 22:19:50 +0100

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,15 @@
-# symlink
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/.zsh ~/.zsh
+#!/bin/sh
 
-ln -s ~/.dotfiles/.config/* ~/.config
-ln -s ~/.dotfiles/scripts/* "/usr/local/bin"
+#install fish
+brew install fish
 
 # install starship
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
-#install ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# symlink
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.config/* ~/.config
+ln -s ~/.dotfiles/scripts/* "/usr/local/bin"
 
 #install micro
 curl https://getmic.ro | bash
