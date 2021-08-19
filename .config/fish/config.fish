2543fe1008c70505cb6b8843bc2de67e3fcb33de
wouter <wouter@debruijn.dev>
2021-08-19 13:56:53 +0200

    Updated config.fish and unhide script

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,8 @@ eval (/opt/homebrew/bin/brew shellenv)
 starship init fish | source
 
 export PATH="$HOME/.cargo/bin:$PATH"
+export PF_COL1="6" PF_ALIGN="3" PF_COL3="6" PF_COL4="1"
 source ~/.dotfiles/misc/aliases.sh
 
-clear
-afetch
+clear 
+pfetch
