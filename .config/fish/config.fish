310674ba1296eb96458dcaf849f0806f0cc7f15b
wouter <wouter@debruijn.dev>
2021-10-31 15:47:24 +0100

    added aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,10 @@
 eval (/opt/homebrew/bin/brew shellenv)
 starship init fish | source
 
-export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="/usr/local/bin:$HOME/.cargo/bin:$PATH"
 export PF_COL1="6" PF_ALIGN="3" PF_COL3="6" PF_COL4="1"
 source ~/.dotfiles/misc/aliases.sh
 
 clear 
 pfetch
+fish_add_path /opt/homebrew/opt/libpq/bin
